96995394bff5213a483ce797d16484aa2d357757
Mohan Ambalavanan <mohan.ambalavanan@gmail.com>
2012-10-05 12:57:04 +1000

    Added comments

--- yumhelper.py
+++ yumhelper.py
@@ -2,10 +2,23 @@ __author__ = 'mohan'
 import  yum;
 import  sys;
 
-#A simple YumHelper class based on
-# http://yum.baseurl.org/api/yum/
-#Notice :Write or Delete operations require subsequent permissions
 class YumHelper():
+    """
+        A simple YumHelper class based on
+         http://yum.baseurl.org/api/yum/
+         For a given rpm name it allows you to query and
+         find if it is installed/available/updateable in your configured
+         yum repo
+         NOTICE:
+                Write or Delete operations require root or super permissions
+
+                Works only on YUM based machines such as Cent-OS,RHEL etc..
+                In debian,  you could install createrepo,yum,rpm from aptitude
+                and configure /etc/yum/yum.conf and configure your own
+                yum repository using createrepo
+
+        """
+
     def __init__(self, rpm_name):
         self._rpm_name = rpm_name;
         self._yb = yum.YumBase();
@@ -27,7 +40,7 @@ class YumHelper():
         """
         return self._get_first_item_or_none(self._do_package_list('installed').installed);
 
-    def get_updates(self):
+    def get_update_package(self):
         """
               Returns the latest version of the given package object
               for the given rpm name
@@ -45,13 +58,23 @@ class YumHelper():
             return None;
 
     #TODO :Learn and use lambdas to avoid ugly repetition
+
     def do_install(self, pkg_obj):
+        """
+            Installs the  given pkg obj to your local yum repo
+            requires special  permission for this operation
+        """
         self._yb.doTsSetup();
         self._yb.install(pkg_obj);
         self._yb.buildTransaction();
         self._yb.processTransaction();
 
     def do_remove(self, pkg_obj):
+        """
+            Removes the  given pkg obj to your local yum repo
+            requires special  permission for this operation
+        """
+
         self._yb.doTsSetup();
         self._yb.remove(pkg_obj);
         self._yb.buildTransaction();
@@ -59,6 +82,11 @@ class YumHelper():
 
 
     def do_update(self, pkg_obj):
+        """
+            Updates the  given pkg obj to your local yum repo
+            requires special  permission for this operation
+        """
+
         self._yb.doTsSetup();
         self._yb.update(pkg_obj);
         self._yb.buildTransaction();
@@ -67,7 +95,7 @@ class YumHelper():
 
 def main():
     yum = YumHelper("rpm-test");
-    pkg = yum.get_available_package();
+    pkg = yum.get_update_package();
     yum.do_update(pkg);
 
 
